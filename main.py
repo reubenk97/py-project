f69a9ece5dbe6069fb644e50d1dab94af7036bd9
Reuben Kuruvila <reubenk97@gmail.com>
2023-10-18 19:03:32 -0500

    add flask and route

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-msg = "hello there"
-print (msg)
+from flask import Flask
+app = Flask(__name__)
+
+
+def hello():
+    return 'Hello!'
+app.add_url_rule('/hi', 'hello', hello)
+
+if __name__ == '__main__':
+    app.run(debug = True)
